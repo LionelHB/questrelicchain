b8276417d02c52264c4eeae3c1e230d7b0620a5f
LionelHB <shinoushin@gmail.com>
2023-05-08 22:33:57 +0200

    123

--- public/script/script.js
+++ public/script/script.js
@@ -1,15 +1,9 @@
 //MENU FROM BURGER ***********************
 // **************************************
-let myMenuBurger = document.getElementById("sidenav");
-let myNav = document.querySelector(".mynav");
+let myMenuBurger = document.getElementById("ulscript");
 let myBurger = document.getElementById("myburger");
 let isMenuOpen = false;
 
-document.addEventListener("click", function (event) {
-  if (!myNav.contains(event.target) && !myBurger.contains(event.target)) {
-    closeMenu();
-  }
-});
 
 myBurger.addEventListener("click", function () {
   if (!isMenuOpen) {
@@ -20,90 +14,15 @@ myBurger.addEventListener("click", function () {
 });
 
 function openMenu() {
-  myMenuBurger.style.display = "block";
-  myNav.style.width = "120px";
-  myNav.style.height = "350px";
+  myMenuBurger.style.display = "flex";
   myBurger.style.rotate = "90deg";
   isMenuOpen = true;
 }
 
 function closeMenu() {
   myMenuBurger.style.display = "none";
-  myNav.style.width = "50px";
-  myNav.style.height = "50px";
-  myBurger.style.backgroundPosition = "right";
-  myBurger.style.transform = "translateX(0px)";
+  myBurger.style.rotate = "0deg";
   isMenuOpen = false;
-}
-
-// //***Sticky Nav*** */
-// window.addEventListener("scroll", function () {
-//   let myBarNav = document.querySelector(".mynav");
-//   let scrollPosition = window.scrollY;
-//   let stickyPosition = 80;
-//   let unstickPosition = "100%";
-
-//   if (scrollPosition > stickyPosition) {
-//     myBarNav.classList.add("stickynav");
-//   } else {
-//     myBarNav.classList.remove("stickynav");
-//   }
-
-//   if (scrollPosition > unstickPosition) {
-//     myBarNav.classList.remove("stickynav");
-//   }
-// });
-
-// //SOCIAL MEDIA ***********************
-// // **************************************
-// let socialmedialogo = document.querySelectorAll('.socialmedialogo');
-
-// document.getElementById("socialmedialogo").addEventListener("click", function () {
-
-//   for (i = 0; i < socialmedialogo.length; i++)
-//     if (window.innerWidth > "768px") {
-//       if (socialmedialogo[i].style.visibility === "visible") {
-//         socialmedialogo[i].style.transform = "translateX(-1000px)";
-//         socialmedialogo[i].style.visibility = "hidden";
-//       } else {
-//         socialmedialogo[i].style.visibility = "visible";
-//         socialmedialogo[i].style.transform = "translateX(0px)";
-//       }
-
-//     } else {
-//       socialmedialogo[i].style.transform = "translateX(1000px)";
-//       if (socialmedialogo[i].style.visibility === "visible") {
-//         socialmedialogo[i].style.transform = "translateX(1000px)";
-//         socialmedialogo[i].style.visibility = "hidden";
-//       } else {
-//         socialmedialogo[i].style.visibility = "visible";
-//         socialmedialogo[i].style.transform = "translateX(0px)";
-//       }
-//     }
-// });
-
-//SOCIAL MEDIA Sticky ***********************
-// **************************************
-if (window.innerWidth > "768px") {
-  (function () {
-    let objNav = document.querySelector("#socialmedianav");
-    let memoPositionNav = objNav.getBoundingClientRect().top + window.pageYOffset;
-    function sticky() {
-      let posCurseur = window.pageYOffset;
-      if (posCurseur >= memoPositionNav) {
-        objNav.style.position = "fixed";
-        objNav.style.top = 0;
-        objNav.style.left = "-1.6rem";
-        objNav.style.zIndex = 999;
-      } else {
-        objNav.style.position = "absolute";
-        objNav.style.top = "auto";
-        objNav.style.left = "2rem";
-        objNav.style.zIndex = "auto";
-      }
-    }
-    window.addEventListener("scroll", sticky);
-  })();
 }
 
 //POPUP HEADER ***********************
@@ -170,14 +89,5 @@ if (window.innerWidth > "768px") {
   });
 }
 
-//OVERLAY LAST NFT***********************
-// **************************************
 
-// document.getElementById('overlay').addEventListener('click', function () {
-
-//   scaleLastNft.style.position = "static";
-//   document.getElementById('overlay').style.display = 'none';
-//   scaleLastNft.style.transform = "translateX(0px) translateY(0px)";
-//   scaleLastNft.style.scale = 1;
-// });
 
